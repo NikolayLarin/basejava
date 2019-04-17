14b4c779d3e6c0cee76af6bebb0d2764e648e43b
Nikolay Larin <nikolay.larin.itmail@gmail.com>
2019-04-17 13:44:51 +0300

    Class ArrayStorage methods implemented.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -7,45 +7,48 @@ public class ArrayStorage {
     private Resume[] storage = new Resume[10000];
 
     void clear() {
-        for (int i = 0; i < size(); i++) {
+        int size = size();
+        for (int i = 0; i < size; i++) {
             this.storage[i] = null;
         }
     }
 
     void save(Resume resume) {
-        this.storage[size() - 1] = resume;
+        this.storage[size()] = resume;
     }
 
     Resume get(String uuid) {
-//        return storage[findElementNumber(uuid)];
-        int size = size();
-        for (int i = 0; i < size; i++) {
-            if (storage[i].toString().equals(uuid)) {
-                return storage[i];
-            }
+        int i = findElementIndex(uuid);
+        if (i >= 0) {
+            return storage[i];
+        } else {
+            Resume notPresent = new Resume();
+            notPresent.uuid = "Элемент с uuid, равным <" + uuid + ">, не найден.";
+            return notPresent;
         }
-        System.out.println("Элемент с uuid, равным " + uuid + " не найден.");
-        return null;
     }
 
     void delete(String uuid) {
-//        for (int i = findElementNumber(uuid); i <= size() ; i++) {
-////            storage[i] = storage[i++];
-//        }
-    }
-//    private int findElementNumber(String uuid) {
-//        int i = 0;
-//        while (i < size() - 1) {
-//            if (!storage[i].toString().equals(uuid)) {
-//                i++;
+        int i = findElementIndex(uuid);
+        int size = size();
+        if (i >= 0) {
+            System.arraycopy(storage, i + 1, storage, i, size - i);
+//            for (int j = i; j < size; j++) {
+//                storage[j] = storage[j + 1];
 //            }
-//        }
-//        if (i == size() - 1 && !storage[i].toString().equals(uuid)) {
-//            System.out.println("Элемент с uuid, равным " + uuid + " не найден.");
-//            return 0;
-//        }
-//        return i;
-//    }
+        } else {
+            System.out.println("Элемент с uuid, равным <" + uuid + ">, не найден.");
+        }
+    }
+
+    private int findElementIndex(String uuid) {
+        int size = size();
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                return i;
+            }
+        } return -1;
+    }
 
     /**
      * @return array, contains only Resumes in storage (without null)
@@ -55,11 +58,11 @@ public class ArrayStorage {
     }
 
     int size() {
-        int size = 0;
-        while (storage[size] != null) {
-                size++;
-        }
-        System.out.println(size);
-        return size + 1;
+        int i = 0;
+        do {
+            i++;
+        } while (storage[i - 1] != null);
+//        System.out.println(i - 1);
+        return i - 1;
     }
 }
