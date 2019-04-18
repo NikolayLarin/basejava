670c8494909d037b2670370531546525bbb3b626
NikolayLarin <nikolay.larin.itmail@gmail.com>
2019-04-18 23:44:05 +0300

    It does not matter

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -2,7 +2,7 @@
  * Test for your ArrayStorage implementation
  */
 public class MainTestArrayStorage {
-    static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+    private static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) {
         Resume r1 = new Resume();
@@ -12,11 +12,46 @@ public class MainTestArrayStorage {
         Resume r3 = new Resume();
         r3.uuid = "uuid3";
 
+        saveThreeResumes(r1, r2, r3);
+        checkArrayStorageMethods(r1, r2, r3);
+        System.out.println("_________________________");
 
+        Resume r1Upd = new Resume();
+        r1Upd.uuid = "uuid2";
+        Resume r4 = new Resume();
+        r4.uuid = "uuid4";
+        Resume r3Upd = new Resume();
+        r3Upd.uuid = "uuid3";
+
+        saveThreeResumes(r1Upd, r4, r3Upd);
+        ARRAY_STORAGE.update(r1Upd);
+        ARRAY_STORAGE.update(r3Upd);
+        checkArrayStorageMethods(r1Upd, r4, r3Upd);
+        System.out.println("_________________________");
+
+
+
+//        System.out.println("\nGet r2 with uuid <" + ARRAY_STORAGE.get(r2.uuid) + ">");
+//        ARRAY_STORAGE.update(r2Upd);
+//        System.out.println("and update r2 with <" + ARRAY_STORAGE.get(r2.uuid) + "> to r2Upd with <" + r2Upd.toString() + ">:");
+//        System.out.println("Get r2Upd: " + ARRAY_STORAGE.get(r2Upd.uuid));
+//        printAll();
+    }
+
+    private static void printAll() {
+        System.out.println("\nGet All");
+        for (Resume r : ARRAY_STORAGE.getAll()) {
+            System.out.println(r);
+        }
+    }
+
+    private static void saveThreeResumes(Resume r1, Resume r2, Resume r3) {
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
+    }
 
+    private static void checkArrayStorageMethods(Resume r1, Resume r2, Resume r3) {
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.uuid));
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
@@ -29,27 +64,6 @@ public class MainTestArrayStorage {
         printAll();
 
         System.out.println("Size: " + ARRAY_STORAGE.size());
-
-        ARRAY_STORAGE.save(r1);
-        ARRAY_STORAGE.save(r2);
-        ARRAY_STORAGE.save(r3);
-
-        Resume r2Upd = new Resume();
-        r2Upd.uuid = "uuid2";
-        Resume r4 = new Resume();
-        r4.uuid = "uui4";
-
-        System.out.println("\nGet r2 with uuid <" + ARRAY_STORAGE.get(r2.uuid) + ">");
-        ARRAY_STORAGE.update(r2Upd);
-        System.out.println("and update r2 with <" + ARRAY_STORAGE.get(r2.uuid) + "> to r2Upd with <" + r2Upd.toString() + ">:");
-        System.out.println("Get r2Upd: " + ARRAY_STORAGE.get(r2Upd.uuid));
-        printAll();
     }
 
-    static void printAll() {
-        System.out.println("\nGet All");
-        for (Resume r : ARRAY_STORAGE.getAll()) {
-            System.out.println(r);
-        }
-    }
 }
