57c579a551e7e38dc20345683faf5081135edcaf
Nikolay Larin <nikolay.larin.itmail@gmail.com>
2019-05-08 18:02:59 +0300

    HW_05. JInit MapStorageTest class implemented.

---
+++ test/ru/javawebinar/basejava/storage/MapStorageTest.java
@@ -0,0 +1,31 @@
+package ru.javawebinar.basejava.storage;
+
+import org.junit.Test;
+import ru.javawebinar.basejava.exception.StorageException;
+import ru.javawebinar.basejava.model.Resume;
+
+import static org.junit.Assert.fail;
+
+public class MapStorageTest extends AbstractArrayStorageTest {
+
+    public MapStorageTest() {
+        super(new MapStorage());
+    }
+
+    @Override
+    @Test
+    public void saveOverflow() {
+        storage.clear();
+        int i = 0;
+        try {
+            for (i = 0; i < AbstractArrayStorage.STORAGE_LIMIT; i++) {
+                storage.save(new Resume());
+            }
+        } catch (StorageException e) {
+            fail("RuntimeException: storage overflow in AbstractArrayStorage, " +
+                    "storage limit <" + i + "> reached");
+        }
+        assertSize(AbstractArrayStorage.STORAGE_LIMIT);
+        storage.save(new Resume());
+    }
+}
