4d553e1b7c2d868693dd88b115356ec2bbe4c2b5
Nikolay Larin <nikolay.larin.itmail@gmail.com>
2019-05-08 14:37:12 +0300

    HW_05. MapStorage implementation started

---
+++ src/ru/javawebinar/basejava/storage/MapStorage.java
@@ -0,0 +1,62 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.model.Resume;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+/**
+ * List based storage for Resumes
+ */
+public class MapStorage extends AbstractStorage {
+
+    protected final static ArrayList<Resume> storage = new ArrayList<>();
+
+    public int size() {
+        return storage.size();
+    }
+
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    protected void updateResume(Resume resume, int index) {
+        storage.set(index, resume);
+    }
+
+    @Override
+    protected void saveResume(Resume r, int index) {
+        storage.add(r);
+    }
+
+    @Override
+    protected Resume getResume(int index) {
+        return storage.get(index);
+    }
+
+    @Override
+    protected void deleteResume(int index) {
+        storage.remove(index);
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        int size = storage.size();
+        return storage.toArray(new Resume[size]);
+    }
+
+    @Override
+    protected int getIndex(String uuid) {
+        int index = 0;
+        for (Resume r : storage) {
+            if (Objects.equals(r.getUuid(), uuid)) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+}
