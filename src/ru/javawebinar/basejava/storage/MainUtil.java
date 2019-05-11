8778e677edc384297f1dab1131bc7d2973a58705
NikolayLarin <nikolay.larin.itmail@gmail.com>
2019-05-11 14:43:41 +0300

    HW_05. MainUtil class added.

---
+++ src/ru/javawebinar/basejava/storage/MainUtil.java
@@ -0,0 +1,14 @@
+package ru.javawebinar.basejava.storage;
+
+public class MainUtil {
+    public static void main(String[] args) {
+        System.out.println(Integer.valueOf(-1) == Integer.valueOf(-1)); // true
+        System.out.println(Integer.valueOf(-1) == new Integer(-1)); // false
+        int result = getInt();
+        System.out.println(result);
+    }
+
+    private static Integer getInt() {
+        return -1;
+    }
+}
