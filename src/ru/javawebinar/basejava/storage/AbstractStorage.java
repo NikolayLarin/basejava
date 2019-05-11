9ea0cbcb86ec2435a0546a83ba17e41ccd0b5a9d
NikolayLarin <nikolay.larin.itmail@gmail.com>
2019-05-11 13:32:28 +0300

    HW_05. Task analysis and code refactoring: private getExistedSearchKey & getNotExistedSearchKey added.

--- src/ru/javawebinar/basejava/storage/AbstractStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractStorage.java
@@ -6,44 +6,6 @@ import ru.javawebinar.basejava.model.Resume;
 
 public abstract class AbstractStorage implements Storage {
 
-    public void update(Resume r) {
-        String uuid = r.getUuid();
-        Object searchKey = getSearchKey(uuid);
-        if (!isExist(searchKey)) {
-            throw new NotExistStorageException(uuid);
-        } else {
-            updateResume(r, searchKey);
-        }
-    }
-
-    public void save(Resume r) {
-        String uuid = r.getUuid();
-        Object searchKey = getSearchKey(uuid);
-        if (isExist(searchKey)) {
-            throw new ExistStorageException(uuid);
-        } else {
-            saveResume(r, searchKey);
-        }
-    }
-
-    public Resume get(String uuid) {
-        Object searchKey = getSearchKey(uuid);
-        if (!isExist(searchKey)) {
-            throw new NotExistStorageException(uuid);
-        } else {
-            return getResume(searchKey);
-        }
-    }
-
-    public void delete(String uuid) {
-        Object searchKey = getSearchKey(uuid);
-        if (!isExist(searchKey)) {
-            throw new NotExistStorageException(uuid);
-        } else {
-            deleteResume(searchKey);
-        }
-    }
-
     protected abstract void updateResume(Resume r, Object searchKey);
 
     protected abstract void saveResume(Resume r, Object searchKey);
@@ -55,4 +17,40 @@ public abstract class AbstractStorage implements Storage {
     protected abstract boolean isExist(Object searchKey);
 
     protected abstract Resume getResume(Object searchKey);
+
+    public void update(Resume r) {
+        Object searchKey = getExistedSearchKey(r.getUuid());
+        updateResume(r, searchKey);
+    }
+
+    public void save(Resume r) {
+        Object searchKey = getNotExistedSearchKey(r.getUuid());
+        saveResume(r, searchKey);
+    }
+
+    public Resume get(String uuid) {
+        Object searchKey = getExistedSearchKey(uuid);
+        return getResume(searchKey);
+    }
+
+    public void delete(String uuid) {
+        Object searchKey = getExistedSearchKey(uuid);
+        deleteResume(searchKey);
+    }
+
+    private Object getNotExistedSearchKey(String uuid) {
+        Object searchKey = getSearchKey(uuid);
+        if (isExist(searchKey)) {
+            throw new ExistStorageException(uuid);
+        }
+        return searchKey;
+    }
+
+    private Object getExistedSearchKey(String uuid) {
+        Object searchKey = getSearchKey(uuid);
+        if (!isExist(searchKey)) {
+            throw new NotExistStorageException(uuid);
+        }
+        return searchKey;
+    }
 }
