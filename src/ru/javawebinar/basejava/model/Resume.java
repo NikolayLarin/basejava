10771b216b3c399d7ee021bbc2492caefa4bd591
Nikolay Larin <nikolay.larin.itmail@gmail.com>
2019-05-14 14:22:31 +0300

    HW_06. Field fullName added.

--- src/ru/javawebinar/basejava/model/Resume.java
+++ src/ru/javawebinar/basejava/model/Resume.java
@@ -10,16 +10,25 @@ public class Resume {
     // Unique identifier
     private final String uuid;
 
+    private String fullName;
+
     public Resume() {
-        this(UUID.randomUUID().toString());
+        this.uuid = UUID.randomUUID().toString();
     }
 
     public Resume(String uuid) {
         this.uuid = uuid;
     }
-
+    public Resume(String uuid, String fullName) {
+        this.uuid = uuid;
+        this.fullName = fullName;
+    }
     public String getUuid() {
         return uuid;
+    }
+
+    public String getFullName() {
+        return fullName;
     }
 
     @Override
