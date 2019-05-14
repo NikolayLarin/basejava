0c03889435d432d7166d7c5c8b89b85ecd155ced
Nikolay Larin <nikolay.larin.itmail@gmail.com>
2019-05-14 17:31:35 +0300

    HW_06. Implementation started

---
+++ src/ru/javawebinar/basejava/storage/MapUuidStorage.java
@@ -0,0 +1,65 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.model.Resume;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Map based storage for Resumes
+ */
+// TODO implement
+// TODO create new MapStorage with search key not uuid
+public class MapUuidStorage extends AbstractStorage {
+    private Map<String, Resume> map = new HashMap<>();
+
+    @Override
+    protected String getSearchKey(String uuid) {
+        return uuid;
+    }
+
+    @Override
+    protected void doUpdate(Resume r, Object searchKey) {
+    }
+
+    @Override
+    protected boolean isExist(Object searchKey) {
+        return false;
+    }
+
+    @Override
+    protected void doSave(Resume r, Object searchKey) {
+
+    }
+
+    @Override
+    protected Resume doGet(Object searchKey) {
+        return null;
+    }
+
+    @Override
+    protected void doDelete(Object searchKey) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return new Resume[0];
+    }
+
+
+    public List<Resume> getAllSorted() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
