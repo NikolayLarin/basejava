94eb0b28b2d5ed39b1365e9f0b995179f31aa383
Nikolay Larin <nikolay.larin.itmail@gmail.com>
2019-06-03 13:38:14 +0300

    HW_08. Intro

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -6,21 +6,27 @@ import java.io.IOException;
 
 public class MainFile {
     public static void main(String[] args) {
-        File filePath = new File("D:\\Java_Projects\\TopJava.ru\\basejava\\.gitignore");
-
-        File file = new File(".\\.gitignore");
+        File testFilePath = new File("D:\\Java_Projects\\TopJava.ru\\basejava\\.gitignore");
+        String filePath = ".\\.gitignore";
+        File file = new File(filePath);
         try {
-            System.out.println(filePath.getCanonicalPath() + "  " + file.getCanonicalPath());
+            System.out.println(testFilePath.getCanonicalPath() + "  " + file.getCanonicalPath());
         } catch (IOException e) {
             throw new RuntimeException("Error", e);
         }
-        File dir = new File ("./src/ru/javawebinar/basejava");
+        File dir = new File("./src/ru/javawebinar/basejava");
         System.out.println(dir.isDirectory());
         String[] list = dir.list();
-        if (list != null){
+        if (list != null) {
             for (String name : list) {
                 System.out.println(name);
             }
+        }
+
+        try (FileInputStream fis = new FileInputStream(filePath)){
+            System.out.println(fis.read());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
