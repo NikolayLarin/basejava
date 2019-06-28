10746b9d2943c5b547711d2da1736fec3b597a03
NikolayLarin <nikolay.larin.itmail@gmail.com>
2019-06-28 19:33:55 +0300

    HW_09. IOStrategy implemented

--- src/ru/javawebinar/basejava/storage/FileStorage.java
+++ src/ru/javawebinar/basejava/storage/FileStorage.java
@@ -3,17 +3,32 @@ package ru.javawebinar.basejava.storage;
 import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-public class FileStorage extends AbstractFileStorage implements IOStrategy {
+public class FileStorage extends AbstractStorage<File> implements IOStrategy {
+    private File directory;
 
-    public FileStorage(File directory) {
-        super(directory);
+    protected FileStorage(File directory) {
+        Objects.requireNonNull(directory, "Directory can't be null");
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not directory");
+        }
+        if (!directory.canRead() || !directory.canWrite()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not readable/writable");
+        }
+        this.directory = directory;
     }
 
     @Override
@@ -30,5 +45,82 @@ public class FileStorage extends AbstractFileStorage implements IOStrategy {
         } catch (ClassNotFoundException e) {
             throw new StorageException("Can't downcast resume after reading", null, e);
         }
+    }
+
+    @Override
+    protected File getSearchKey(String uuid) {
+        return new File(directory, uuid);
+    }
+
+    @Override
+    protected void doUpdate(Resume resume, File file) {
+        try {
+            doWrite(resume, new BufferedOutputStream(new FileOutputStream(file)));
+        } catch (IOException e) {
+            throw new StorageException("Can't write file " + file.getAbsolutePath(), resume.getUuid(), e);
+        }
+    }
+
+    @Override
+    protected void doSave(Resume resume, File file) {
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new StorageException("Can't create file " + file.getAbsolutePath(), resume.getUuid(), e);
+        }
+        doUpdate(resume, file);
+    }
+
+    @Override
+    protected void doDelete(File file) {
+        if (!file.delete()) {
+            throw new StorageException("Can't delete file ", file.getName());
+        }
+    }
+
+    @Override
+    protected boolean isExist(File file) {
+        return file.exists();
+    }
+
+    @Override
+    protected Resume doGet(File file) {
+        try {
+            return doRead(new BufferedInputStream(new FileInputStream(file)));
+        } catch (IOException e) {
+            throw new StorageException("Can't read file " + file.getAbsolutePath(), file.getName(), e);
+        }
+    }
+
+    @Override
+    protected List<Resume> doCopyAll() {
+        File[] files = directory.listFiles();
+        if (files == null) {
+            throw new StorageException("Can't read directory " + directory.getAbsolutePath(), null);
+        }
+        List<Resume> list = new ArrayList<>(files.length);
+        for (File file : files) {
+            list.add(doGet(file));
+        }
+        return list;
+    }
+
+    @Override
+    public void clear() {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                doDelete(file);
+            }
+        }
+    }
+
+    @Override
+    public int size() {
+        String[] list = directory.list();
+        if (list == null) {
+            throw new StorageException("Directory reed Error", null);
+        }
+        return list.length;
     }
 }
--- src/ru/javawebinar/basejava/storage/ObjectStreamFileStorage.java
+++ src/ru/javawebinar/basejava/storage/ObjectStreamFileStorage.java
@@ -1,34 +1,10 @@
 package ru.javawebinar.basejava.storage;
 
-import ru.javawebinar.basejava.exception.StorageException;
-import ru.javawebinar.basejava.model.Resume;
-
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
 
-public class ObjectStreamFileStorage extends AbstractFileStorage {
+public class ObjectStreamFileStorage extends FileStorage {
 
     public ObjectStreamFileStorage(File directory) {
         super(directory);
-    }
-
-    @Override
-    public void doWrite(Resume resume, OutputStream outputStream) throws IOException {
-        try (ObjectOutputStream oos = new ObjectOutputStream(outputStream)) {
-            oos.writeObject(resume);
-        }
-    }
-
-    @Override
-    public Resume doRead(InputStream inputStream) throws IOException {
-        try (ObjectInputStream ois = new ObjectInputStream(inputStream)) {
-            return (Resume) ois.readObject();
-        } catch (ClassNotFoundException e) {
-            throw new StorageException("Can't downcast resume after reading", null, e);
-        }
     }
 }
