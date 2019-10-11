b1d397083e96fb8ee4870516aca4e7550dcd4a62
NikolayLarin <nikolay.larin.itmail@gmail.com>
2019-10-11 20:27:08 +0300

    HW_17. Deploy on HEROKU
    Config & resumes.properties changed to connect a heroku db on https://resumesappbylarin.herokuapp.com and to create war. Local db connection settings commented.

--- src/ru/javawebinar/basejava/util/Config.java
+++ src/ru/javawebinar/basejava/util/Config.java
@@ -4,19 +4,41 @@ import ru.javawebinar.basejava.storage.SqlStorage;
 import ru.javawebinar.basejava.storage.Storage;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-//    private static final File PROPS = new File("D:/Java/MyProjects/basejava/config/resumes.properties");
-        private static final File PROPS = new File(getHomeDir(), "config/resumes.properties");
+    // private static final File PROPS = new File("D:/Java/MyProjects/basejava/config/resumes.properties");
+/*
+    // settings for local db:
+    private static final File PROPS = new File(getHomeDir(), "config/resumes.properties");
+*/
+
+    // settings for heroku db on https://resumesappbylarin.herokuapp.com:
+    // also mark config folder as resource root
+    private static final String PROPS = "/resumes.properties";
+
     private static final Config INSTANCE = new Config();
 
     private final File storageDir;
+
     private final Storage sqlStorage;
 
+    // settings for heroku db on https://resumesappbylarin.herokuapp.com:
+    private Config() {
+        try (InputStream is = Config.class.getResourceAsStream(PROPS)) {
+            Properties props = new Properties();
+            props.load(is);
+            storageDir = new File(props.getProperty("storage.dir"));
+            sqlStorage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
+        } catch (IOException e) {
+            throw new IllegalStateException("Invalid config file " + PROPS);
+        }
+    }
+
+/*
+    // settings for local db:
     private Config() {
         try (InputStream is = new FileInputStream(PROPS)) {
             Properties props = new Properties();
@@ -27,6 +49,7 @@ public class Config {
             throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
         }
     }
+*/
 
     public static Config getINSTANCE() {
         return INSTANCE;
@@ -40,6 +63,8 @@ public class Config {
         return sqlStorage;
     }
 
+/*
+    // settings for local db:
     private static File getHomeDir() {
         String prop = System.getProperty("homeDir");
         File homeDir = new File(prop == null ? "." : prop);
@@ -48,4 +73,5 @@ public class Config {
         }
         return homeDir;
     }
+*/
 }
